6b7bcdf299387347d5b204acb6b95d0e6e203f2b
xuxin <xu_xin96@163.com>
2020-09-30 11:34:32 +0800

    dva+antd初始化

---
+++ src/components/Example.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button} from 'antd'
+
+const Example = () => {
+  return (
+    <div>
+      Example
+      <Button>12222</Button>
+    </div>
+  );
+};
+
+Example.propTypes = {
+};
+
+export default Example;
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+import IndexPage from './routes/IndexPage';
+import Example from './components/Example'
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={IndexPage} />
+        <Route path="/example" exact component={Example} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default RouterConfig;
